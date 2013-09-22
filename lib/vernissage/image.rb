daa26433a65ec1bf19b073dfb74328fae80105f1
Kiril Tonev <kiril.tonev@gmail.com>
2013-09-22 12:27:00 +0200

    Basic image similarities.

--- lib/vernissage/image.rb
+++ lib/vernissage/image.rb
@@ -1,15 +1,40 @@
 # coding: utf-8
+require 'pathname'
 
 module Vernissage
 
   class Image
 
+    attr_reader :path
+
     def initialize(path)
-      @path = path
+      @path = Pathname.new(path)
+    end
+
+    def basename
+      @path.basename(@path.extname).to_s
+    end
+
+    def name_fragments
+      basename.split(' ')
+    end
+
+    def name_has_multiple_fragments?
+      name_fragments.size > 1
     end
 
     def related_to?(image)
-      false
+      if self.name_has_multiple_fragments?
+        image.name_contained_in_fragments? self.name_fragments
+      else
+        self.name_contained_in_fragments? image.name_fragments
+      end
+    end
+
+    protected
+
+    def name_contained_in_fragments?(fragments)
+      fragments.include? self.basename
     end
 
   end
--- test/test_image.rb
+++ test/test_image.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 require 'vernissage'
+require 'pathname'
 require 'test/unit'
 
 class TestImage < Test::Unit::TestCase
@@ -8,11 +9,32 @@ class TestImage < Test::Unit::TestCase
   include Vernissage
 
   def test_init
-    assert_not_nil Image.new('file.jpeg')
+    assert_not_nil Image.new('image.jpeg')
+    assert_not_nil Image.new(Pathname.new('image.jpg'))
+  end
+
+  def test_basename
+    assert_equal 'IMG_0508', Image.new('IMG_0508').basename
+    assert_equal 'IMG_0494 copy', Image.new('IMG_0494 copy.jpg').basename
+    assert_equal 'P1050541 copy', Image.new('P1050541 copy.jpg').basename
+  end
+
+  def test_fragments
+    assert_equal [ 'IMG_0508' ], Image.new('IMG_0508').name_fragments
+    assert_equal %w{IMG_0508 copy}, Image.new('IMG_0508 copy').name_fragments
+  end
+
+  def name_has_multiple_fragments
+    assert Image.new('IMG_0508 copy').name_has_multiple_fragments?
+    assert !Image.new('IMG_0508').name_has_multiple_fragments?
   end
 
   def test_related_to_smaller_image
     assert Image.new('file.jpeg').related_to? Image.new('file small.jpeg')
+  end
+
+  def test_related_to_copy
+    assert Image.new('09062012967.jpeg').related_to? Image.new('09062012967 copy.jpg')
   end
 
 end
