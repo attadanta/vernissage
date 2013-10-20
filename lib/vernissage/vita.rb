3343e0b188c170ad09de71260153bf4894034bfd
Kiril Tonev <kiril.tonev@gmail.com>
2013-10-20 18:28:16 +0200

    Parse vita.

--- lib/vernissage/vita.rb
+++ lib/vernissage/vita.rb
@@ -4,12 +4,40 @@ module Vernissage
 
   class Vita
 
+    def parse_header(line)
+      line[0...-1]
+    end
+
+    def parse_item(line)
+      leader = line =~ /-/
+      line[(leader+2)..-1]
+    end
+
     def is_item_line(line)
       line.start_with? "\s-"
     end
 
     def is_header_line(line)
       !is_item_line(line) && line.end_with?(":")
+    end
+
+    def parse(contents)
+      vita = {}
+      last_heading = ""
+      contents.each_line do |line|
+        line = line.chomp.strip
+        unless line.empty?
+          if (is_header_line(line))
+            last_heading = parse_header(line)
+          else
+            line = parse_item(line)
+            items = vita.fetch(last_heading, [])
+            items.push line
+            vita[last_heading] = items
+          end
+        end
+      end
+      vita
     end
 
   end
--- test/test_vita.rb
+++ test/test_vita.rb
@@ -7,6 +7,14 @@ class TestVita < Test::Unit::TestCase
 
   include Vernissage
 
+  def test_parses_header
+    assert_equal "Header", Vita.new.parse_header("Header:")
+  end
+
+  def test_parses_item
+    assert_equal "an item", Vita.new.parse_item(" - an item")
+  end
+
   def test_detects_item_line
     assert Vita.new.is_item_line(" - 2013 - Louvre; exhibition")
   end
@@ -29,6 +37,50 @@ class TestVita < Test::Unit::TestCase
 
   def test_detects_content_line_as_false_header_line
     assert !(Vita.new.is_header_line("education"))
+  end
+
+  def test_parses_empty_vita
+    assert_equal({}, Vita.new.parse(""))
+  end
+
+  def test_parses_single_heading
+    contents = <<-END
+    Expertise:
+     - been there
+     - done that
+    END
+    expected = { "Expertise" => [ 'been there', 'done that' ] }
+    assert_equal expected, Vita.new.parse(contents)
+  end
+
+  def test_parse_ignores_gaps
+    contents = <<-END
+    Expertise:
+
+     - been there
+     - done that
+    END
+    expected = { "Expertise" => [ 'been there', 'done that' ] }
+    assert_equal expected, Vita.new.parse(contents)
+  end
+
+  def test_parses_multiple_headings
+    contents = <<-END
+    Life:
+
+     - I am an orphan
+     - on God's highway
+
+    On the bright side:
+
+     - I have had friendships
+     - pure and golden
+    END
+    expected = {
+      "Life" => [ "I am an orphan", "on God's highway" ],
+      "On the bright side" => [ 'I have had friendships', 'pure and golden' ]
+    }
+    assert_equal expected, Vita.new.parse(contents)
   end
 
 end
